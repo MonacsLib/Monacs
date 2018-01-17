562982a2cff9881227a1da8b637d6c6f4e43ba63
Bart Sokol <bartsokol@users.noreply.github.com>
2018-01-17 21:45:03 +0100

    Initial version of Unit

---
+++ Monacs.Core/Unit/Result.Unit.Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Monacs.Core
+{
+    public static partial class Result
+    {
+        public static class Unit
+        {
+            public static Result<Monacs.Core.Unit.Unit> Ok() =>
+                Result.Ok(Monacs.Core.Unit.Unit.Default);
+
+            public static Result<Monacs.Core.Unit.Unit> Error(ErrorDetails error) =>
+                Result.Error<Monacs.Core.Unit.Unit>(error);
+        }
+
+        public static Result<Monacs.Core.Unit.Unit> Ignore<TValue>(this Result<TValue> result) =>
+            result.Map(_ => Monacs.Core.Unit.Unit.Default);
+    }
+}
