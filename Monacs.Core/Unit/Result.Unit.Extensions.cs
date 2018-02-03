0618fd2d062a13a48a18cd67d937db040c4f22c8
Bart Sokol <bartsokol@users.noreply.github.com>
2018-02-03 13:16:58 +0100

    Documentation for Unit type and its extensions

--- Monacs.Core/Unit/Result.Unit.Extensions.cs
+++ Monacs.Core/Unit/Result.Unit.Extensions.cs
@@ -4,14 +4,27 @@ using System.Linq;
 
 namespace Monacs.Core.Unit
 {
+    ///<summary>
+    /// Extensions for Result<Unit> type.
+    ///</summary>
     public static class Result
     {
+        ///<summary>
+        /// Creates successful Result<Unit>.
+        ///</summary>
         public static Result<Unit> Ok() =>
             Core.Result.Ok(Unit.Default);
 
+        ///<summary>
+        /// Creates failed Result<Unit> with provided error details.
+        ///</summary>
         public static Result<Unit> Error(ErrorDetails error) =>
             Core.Result.Error<Unit>(error);
 
+        ///<summary>
+        /// Rejects the value of the Result<T> and returns Result<Unit> instead.
+        /// If the input Result<T> is Error then the error details are preserved.
+        ///</summary>
         public static Result<Unit> Ignore<T>(this Result<T> result) =>
             result.Map(_ => Unit.Default);
     }
--- Monacs.Core/Unit/Unit.cs
+++ Monacs.Core/Unit/Unit.cs
@@ -2,16 +2,35 @@ using System;
 
 namespace Monacs.Core.Unit
 {
+    ///<summary>
+    /// Type that has only one value.
+    /// Used to replace void whenever some value is needed, e.g. you can return Task<Unit>.
+    ///</summary>
     public struct Unit : IEquatable<Unit>
     {
+        ///<summary>
+        /// The only value of Unit.
+        ///</summary>
         public static Unit Default { get; } = default;
 
+        ///<summary>
+        /// Unit is always equal to itself. There is only one possible value of Unit.
+        ///</summary>
         public bool Equals(Unit other) => true;
 
+        ///<summary>
+        /// Unit is only equal to itself.
+        ///</summary>
         public override bool Equals(object obj) => obj is Unit;
 
+        ///<summary>
+        /// Hash Code of Unit is always 0.
+        ///</summary>
         public override int GetHashCode() => 0;
 
+        ///<summary>
+        /// String representation of Unit is ().
+        ///</summary>
         public override string ToString() => "()";
 
         public static bool operator ==(Unit first, Unit second) => true;
