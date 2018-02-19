97c637d7e38a219c56d87234769e5c537cb42613
Paweł Iżycki <pawelizycki@gmail.com>
2018-02-19 20:57:32 +0100

    Implemented Match2 and Match3

--- Monacs.Core/Tuples/Result.Tuple.Extensions.cs
+++ Monacs.Core/Tuples/Result.Tuple.Extensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
 using static Monacs.Core.Result;
 
 namespace Monacs.Core.Tuples
@@ -15,6 +13,7 @@ namespace Monacs.Core.Tuples
         public static Result<TResult> Bind3<TResult, T1, T2, T3>(this Result<(T1, T2, T3)> result, Func<T1, T2, T3, Result<TResult>> func) =>
             result.IsOk ? func(result.Value.Item1, result.Value.Item2, result.Value.Item3) : Error<TResult>(result.Error);
 
+
         /* Map2 */
         public static Result<TResult> Map2<TResult, T1, T2>(this Result<(T1, T2)> result, Func<T1, T2, TResult> func) =>
             result.IsOk ? Ok(func(result.Value.Item1, result.Value.Item2)) : Error<TResult>(result.Error);
@@ -22,5 +21,23 @@ namespace Monacs.Core.Tuples
         /* Map3 */
         public static Result<TResult> Map3<TResult, T1, T2, T3>(this Result<(T1, T2, T3)> result, Func<T1, T2, T3, TResult> func) =>
             result.IsOk ? Ok(func(result.Value.Item1, result.Value.Item2, result.Value.Item3)) : Error<TResult>(result.Error);
+
+
+        /* Match2 */
+
+        public static TVal Match2<TFst, TSnd, TVal>(
+            this Result<(TFst fst, TSnd snd)> result,
+                 Func<TFst, TSnd, TVal> ok,
+                 Func<ErrorDetails, TVal> error) =>
+                      result.IsOk
+                      ? ok(result.Value.fst, result.Value.snd)
+                      : error(result.Error);
+
+        /* Match3 */
+
+        public static TVal Match3<TFst, TSnd, TTrd, TVal>(this Result<(TFst fst, TSnd snd, TTrd trd)> result, Func<TFst, TSnd, TTrd, TVal> ok, Func<ErrorDetails, TVal> error) =>
+            result.IsOk ? ok(result.Value.fst, result.Value.snd, result.Value.trd) : error(result.Error);
+
+
     }
 }
