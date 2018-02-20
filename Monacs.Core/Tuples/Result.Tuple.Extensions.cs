7e0f8ec7b62283973f8a9988317545f0911b9bda
Paweł Iżycki <pawelizycki@gmail.com>
2018-02-20 19:58:22 +0100

    Result.TryCatch2 and Result.TryCatch3

--- Monacs.Core/Tuples/Result.Tuple.Extensions.cs
+++ Monacs.Core/Tuples/Result.Tuple.Extensions.cs
@@ -6,8 +6,8 @@ namespace Monacs.Core.Tuples
     public static class Result
     {
         /* Bind2 */
-        public static Result<TResult> Bind2<TResult, T1, T2>(this Result<(T1, T2)> result, Func<T1, T2, Result<TResult>> func) =>
-            result.IsOk ? func(result.Value.Item1, result.Value.Item2) : Error<TResult>(result.Error);
+        public static Result<TValue> Bind2<TValue, TFst, T2>(this Result<(TFst, T2)> result, Func<TFst, T2, Result<TValue>> func) =>
+            result.IsOk ? func(result.Value.Item1, result.Value.Item2) : Error<TValue>(result.Error);
 
         /* Bind3 */
         public static Result<TResult> Bind3<TResult, T1, T2, T3>(this Result<(T1, T2, T3)> result, Func<T1, T2, T3, Result<TResult>> func) =>
@@ -58,5 +58,42 @@ namespace Monacs.Core.Tuples
                 action(result.Value.fst, result.Value.snd, result.Value.trd);
             return result;
         }
+
+        /* Try/Catch */
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <typeparam name="TFst"></typeparam>
+        /// <typeparam name="TSnd"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="tryFunc"></param>
+        /// <param name="errorHandler"></param>
+        /// <returns></returns>
+        public static Result<TValue> TryCatch2<TValue, TFst, TSnd>(
+            this Result<(TFst fst, TSnd snd)> result,
+                 Func<TFst, TSnd, TValue> tryFunc,
+                 Func<TFst, TSnd, Exception, ErrorDetails> errorHandler) =>
+                    result.Bind(value => TryCatch(func: () => tryFunc(value.fst, value.snd),
+                                                  errorHandler: err => errorHandler(value.fst, value.snd, err)));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <typeparam name="TFst"></typeparam>
+        /// <typeparam name="TSnd"></typeparam>
+        /// <typeparam name="TTrd"></typeparam>
+        /// <param name="result"></param>
+        /// <param name="tryFunc"></param>
+        /// <param name="errorHandler"></param>
+        /// <returns></returns>
+        public static Result<TValue> TryCatch3<TValue, TFst, TSnd, TTrd>(
+            this Result<(TFst fst, TSnd snd, TTrd trd)> result,
+                 Func<TFst, TSnd, TTrd, TValue> tryFunc,
+                 Func<TFst, TSnd, TTrd, Exception, ErrorDetails> errorHandler) =>
+                     result.Bind(value => TryCatch(func: () => tryFunc(value.fst, value.snd, value.trd),
+                                                   errorHandler: err => errorHandler(value.fst, value.snd, value.trd, err)));
     }
 }
