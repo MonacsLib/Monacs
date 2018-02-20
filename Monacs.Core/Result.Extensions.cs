890b2a83c8d979c607f2cef239b867d51f3796c1
Bart Sokol <bartsokol@users.noreply.github.com>
2018-02-20 20:37:16 +0100

    Documentation for Result constructors and converters

--- Monacs.Core/Result.Extensions.cs
+++ Monacs.Core/Result.Extensions.cs
@@ -4,54 +4,199 @@ using System.Linq;
 
 namespace Monacs.Core
 {
+    /// <summary>
+    /// Contains the set of extensions to work with the <see cref="Result{T}"/> type.
+    /// </summary>
     public static partial class Result
     {
         /* Constructors */
 
+        /// <summary>
+        /// Creates the Ok case instance of the <see cref="Result{T}"/> type, encapsulating provided value.
+        /// </summary>
+        /// <typeparam name="T">Type of the encapsulated value.</typeparam>
+        /// <param name="value">The value to encapsulate.</param>
         public static Result<T> Ok<T>(T value) => new Result<T>(value);
 
+        /// <summary>
+        /// Creates the Error case instance of the <see cref="Result{T}"/> type, containing error instead of value.
+        /// </summary>
+        /// <typeparam name="T">Desired type parameter for <see cref="Result{T}"/> type.</typeparam>
+        /// <param name="error">Details of the error.</param>
         public static Result<T> Error<T>(ErrorDetails error) => new Result<T>(error);
 
         /* Converters */
 
+        /// <summary>
+        /// Converts the value of class T to the <see cref="Result{T}"/> type.
+        /// If the value is null, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <typeparam name="T">Type of the encapsulated value.</typeparam>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="error">Details of the error if the value is null.</param>
         public static Result<T> OfObject<T>(T value, ErrorDetails error) where T : class =>
             value != null ? Ok(value) : Error<T>(error);
 
+        /// <summary>
+        /// Converts the value of class T to the <see cref="Result{T}"/> type.
+        /// If the value is null, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <typeparam name="T">Type of the encapsulated value.</typeparam>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="errorFunc">Function yielding details of the error if the value is null.</param>
         public static Result<T> OfObject<T>(T value, Func<ErrorDetails> errorFunc) where T : class =>
             value != null ? Ok(value) : Error<T>(errorFunc());
 
+        /// <summary>
+        /// Converts the value of class T to the <see cref="Result{T}"/> type.
+        /// If the value is null, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <remarks>Extension method variant of <see cref="Result.OfObject{T}(T, ErrorDetails)"/></remarks>
+        /// <typeparam name="T">Type of the encapsulated value.</typeparam>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="error">Details of the error if the value is null.</param>
         public static Result<T> ToResult<T>(this T value, ErrorDetails error) where T : class => OfObject(value, error);
 
+        /// <summary>
+        /// Converts the value of class T to the <see cref="Result{T}"/> type.
+        /// If the value is null, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <remarks>Extension method variant of <see cref="Result.OfObject{T}(T, Func{ErrorDetails})"/></remarks>
+        /// <typeparam name="T">Type of the encapsulated value.</typeparam>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="errorFunc">Function yielding details of the error if the value is null.</param>
         public static Result<T> ToResult<T>(this T value, Func<ErrorDetails> errorFunc) where T : class => OfObject(value, errorFunc);
 
+        /// <summary>
+        /// Converts the value of <see cref="Nullable{T}"/> to the <see cref="Result{T}"/> type.
+        /// If the value is null, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <typeparam name="T">Type of the encapsulated value.</typeparam>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="error">Details of the error if the value is null.</param>
         public static Result<T> OfNullable<T>(T? value, ErrorDetails error) where T : struct =>
             value.HasValue ? Ok(value.Value) : Error<T>(error);
 
+        /// <summary>
+        /// Converts the value of <see cref="Nullable{T}"/> to the <see cref="Result{T}"/> type.
+        /// If the value is null, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <typeparam name="T">Type of the encapsulated value.</typeparam>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="errorFunc">Function yielding details of the error if the value is null.</param>
         public static Result<T> OfNullable<T>(T? value, Func<ErrorDetails> errorFunc) where T : struct =>
             value.HasValue ? Ok(value.Value) : Error<T>(errorFunc());
 
+        /// <summary>
+        /// Converts the value of <see cref="Nullable{T}"/> to the <see cref="Result{T}"/> type.
+        /// If the value is null, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <remarks>Extension method variant of <see cref="Result.OfNullable{T}(T?, ErrorDetails)"/></remarks>
+        /// <typeparam name="T">Type of the encapsulated value.</typeparam>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="error">Details of the error if the value is null.</param>
         public static Result<T> ToResult<T>(this T? value, ErrorDetails error) where T : struct => OfNullable(value, error);
 
+        /// <summary>
+        /// Converts the value of <see cref="Nullable{T}"/> to the <see cref="Result{T}"/> type.
+        /// If the value is null, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <remarks>Extension method variant of <see cref="Result.OfNullable{T}(T?, Func{ErrorDetails})"/></remarks>
+        /// <typeparam name="T">Type of the encapsulated value.</typeparam>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="errorFunc">Function yielding details of the error if the value is null.</param>
         public static Result<T> ToResult<T>(this T? value, Func<ErrorDetails> errorFunc) where T : struct => OfNullable(value, errorFunc);
 
+        /// <summary>
+        /// Converts the string value to the <see cref="Result{T}"/> type.
+        /// If the value is null or empty string, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="error">Details of the error if the value is null or empty.</param>
         public static Result<string> OfString(string value, ErrorDetails error) =>
             string.IsNullOrEmpty(value) ? Error<string>(error) : Ok(value);
 
+        /// <summary>
+        /// Converts the string value to the <see cref="Result{T}"/> type.
+        /// If the value is null or empty string, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="errorFunc">Function yielding details of the error if the value is null or empty.</param>
         public static Result<string> OfString(string value, Func<ErrorDetails> errorFunc) =>
             string.IsNullOrEmpty(value) ? Error<string>(errorFunc()) : Ok(value);
 
+        /// <summary>
+        /// Converts the string value to the <see cref="Result{T}"/> type.
+        /// If the value is null or empty string, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <remarks>Extension method variant of <see cref="Result.OfString(string, ErrorDetails)"/></remarks>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="error">Details of the error if the value is null or empty.</param>
         public static Result<string> ToResult(this string value, ErrorDetails error) => OfString(value, error);
 
+        /// <summary>
+        /// Converts the string value to the <see cref="Result{T}"/> type.
+        /// If the value is null or empty string, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <remarks>Extension method variant of <see cref="Result.OfString(string, Func{ErrorDetails})"/></remarks>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="errorFunc">Function yielding details of the error if the value is null or empty.</param>
         public static Result<string> ToResult(this string value, Func<ErrorDetails> errorFunc) => OfString(value, errorFunc);
 
+        /// <summary>
+        /// Converts the value of <see cref="Option{T}"/> to the <see cref="Result{T}"/> type.
+        /// If the value is None case, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <typeparam name="T">Type of the encapsulated value.</typeparam>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="error">Details of the error if the value is None.</param>
         public static Result<T> OfOption<T>(Option<T> value, ErrorDetails error) where T : struct =>
             value.IsSome ? Ok(value.Value) : Error<T>(error);
 
+        /// <summary>
+        /// Converts the value of <see cref="Option{T}"/> to the <see cref="Result{T}"/> type.
+        /// If the value is None case, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <typeparam name="T">Type of the encapsulated value.</typeparam>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="errorFunc">Function yielding details of the error if the value is None.</param>
         public static Result<T> OfOption<T>(Option<T> value, Func<ErrorDetails> errorFunc) where T : struct =>
             value.IsSome ? Ok(value.Value) : Error<T>(errorFunc());
 
+        /// <summary>
+        /// Converts the value of <see cref="Option{T}"/> to the <see cref="Result{T}"/> type.
+        /// If the value is None case, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <remarks>Extension method variant of <see cref="Result.OfOption{T}(Option{T}, ErrorDetails)"/></remarks>
+        /// <typeparam name="T">Type of the encapsulated value.</typeparam>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="error">Details of the error if the value is None.</param>
         public static Result<T> ToResult<T>(this Option<T> value, ErrorDetails error) where T : struct => OfOption(value, error);
 
+        /// <summary>
+        /// Converts the value of <see cref="Option{T}"/> to the <see cref="Result{T}"/> type.
+        /// If the value is None case, the Error case is yielded.
+        /// Otherwise Ok case with provided value is returned.
+        /// </summary>
+        /// <remarks>Extension method variant of <see cref="Result.OfOption{T}(Option{T}, Func{ErrorDetails})"/></remarks>
+        /// <typeparam name="T">Type of the encapsulated value.</typeparam>
+        /// <param name="value">The value to convert to <see cref="Result{T}"/>.</param>
+        /// <param name="errorFunc">Function yielding details of the error if the value is None.</param>
         public static Result<T> ToResult<T>(this Option<T> value, Func<ErrorDetails> errorFunc) where T : struct => OfOption(value, errorFunc);
 
         /* TryGetResult */
