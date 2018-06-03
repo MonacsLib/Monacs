f871cf2c19c99778c1f0c9548b6f4af676cde4bd
Paweł Iżycki <pawelizycki@gmail.com>
2018-06-03 19:36:28 +0200

    Add more Either extension methods

--- Monacs.Core/Either.cs
+++ Monacs.Core/Either.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Monacs.Core
 {
@@ -56,9 +57,73 @@ namespace Monacs.Core
 
     public enum LeftOrRight { Left, Right }
 
-    public static class EitherExtensions
+    public static partial class Either
     {
-        public static Result Match<L, R, Result>(this Either<L, R> target, Func<L, Result> left, Func<R, Result> right) =>
+        /* Match */
+
+        public static Out Match<L, R, Out>(this Either<L, R> target, Func<L, Out> left, Func<R, Out> right) =>
             target.IsLeft ? left(target.Left) : right(target.Right);
+
+        /* Map */
+
+        public static Out MapRight<L, R, Out>(this Either<L, R> target, Func<R, Out> map) =>
+            map(target.Right);
+
+        public static Out MapLeft<L, R, Out>(this Either<L, R> target, Func<L, Out> map) =>
+            map(target.Left);
+
+        /* Bind */
+
+        public static Either<LOut, ROut> Bind<L, R, LOut, ROut>(
+            this Either<L, R> target, Func<L, LOut> leftBinder, Func<R, ROut> rightBinder) =>
+            target.IsLeft ? Left<LOut, ROut>(leftBinder(target.Left)) : Right<LOut, ROut>(rightBinder(target.Right));
+
+
+        /* Side effects */
+
+        public static Either<L, R> WhenLeft<L, R>(this Either<L, R> target, Action<L> action)
+        {
+            if (target.IsLeft)
+                action(target.Left);
+
+            return target;
+        }
+
+        public static Either<L, R> WhenRight<L, R>(this Either<L, R> target, Action<R> action)
+        {
+            if (target.IsRight)
+                action(target.Right);
+
+            return target;
+        }
+
+        /* Factories */
+
+        public static Either<L, R> Left<L, R>(L left) => new Either<L, R>(left);
+
+        public static Either<L, R> Right<L, R>(R right) => new Either<L, R>(right);
+
+        public static Either<L, R> FromTuple<L, R>((L left, R right) tuple) =>
+            tuple.right == null ? Left<L, R>(tuple.left) : Right<L, R>(tuple.right);
+
+        /* Collections */
+
+        public static IEnumerable<L> ChooseLeft<L, R>(this IEnumerable<Either<L, R>> target) =>
+            target.Where(x => x.LeftOrRight == LeftOrRight.Left).Select(x => x.Left);
+
+        public static IEnumerable<R> ChooseRight<L, R>(this IEnumerable<Either<L, R>> target) =>
+            target.Where(x => x.LeftOrRight == LeftOrRight.Right).Select(x => x.Right);
+
+        public static Option<IEnumerable<L>> SequenceLeft<L, R>(this IEnumerable<Either<L, R>> target) =>
+            target.Any(x => x.IsRight)
+                ? Option.Some(target.ChooseLeft())
+                : Option.None<IEnumerable<L>>();
+
+        public static Option<IEnumerable<R>> SequenceRight<L, R>(this IEnumerable<Either<L, R>> target) =>
+            target.Any(x => x.IsLeft)
+                ? Option.Some(target.ChooseRight())
+                : Option.None<IEnumerable<R>>();
+
+
     }
 }
