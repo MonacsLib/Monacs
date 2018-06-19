2326405601e0af6e031e6166104d7e5d07fd583a
Paweł Iżycki <pawelizycki@gmail.com>
2018-06-19 12:29:25 +0200

    Rename all generic types to match project convention

--- Monacs.Core/Either.cs
+++ Monacs.Core/Either.cs
@@ -4,83 +4,83 @@ using System.Linq;
 
 namespace Monacs.Core
 {
-    public readonly struct Either<L, R> : IEquatable<Either<L, R>>
+    public enum LeftOrRight { Left, Right }
+
+    public readonly struct Either<TLeft, TRight> : IEquatable<Either<TLeft, TRight>>
     {
-        public L LeftValue { get; }
-        public R RightValue { get; }
+        public TLeft LeftValue { get; }
+        public TRight RightValue { get; }
         public LeftOrRight LeftOrRight { get; }
         public bool IsLeft => LeftOrRight == LeftOrRight.Left;
         public bool IsRight => LeftOrRight == LeftOrRight.Right;
 
-        private Either(L left, R right, LeftOrRight leftOrRight)
+        private Either(TLeft left, TRight right, LeftOrRight leftOrRight)
         {
             LeftValue = left;
             RightValue = right;
             LeftOrRight = leftOrRight;
         }
 
-        public Either(L left) : this(left, default, LeftOrRight.Left) { }
-        public Either(R right) : this(default, right, LeftOrRight.Right) { }
+        public Either(TLeft left) : this(left, default, LeftOrRight.Left) { }
+        public Either(TRight right) : this(default, right, LeftOrRight.Right) { }
 
-        public bool Equals(Either<L, R> other) =>
+        public bool Equals(Either<TLeft, TRight> other) =>
             LeftOrRight == other.LeftOrRight
-            && IsLeft ? EqualityComparer<L>.Default.Equals(LeftValue, other.LeftValue)
-                      : EqualityComparer<R>.Default.Equals(RightValue, other.RightValue);
+            && IsLeft ? EqualityComparer<TLeft>.Default.Equals(LeftValue, other.LeftValue)
+                      : EqualityComparer<TRight>.Default.Equals(RightValue, other.RightValue);
 
         public override bool Equals(object obj) =>
             !ReferenceEquals(null, obj)
-            && obj is Either<L, R> either && Equals(either);
+            && obj is Either<TLeft, TRight> either && Equals(either);
 
         public override int GetHashCode()
         {
             unchecked
             {
-                var hashCode = EqualityComparer<L>.Default.GetHashCode(LeftValue);
-                hashCode = (hashCode * 397) ^ EqualityComparer<R>.Default.GetHashCode(RightValue);
+                var hashCode = EqualityComparer<TLeft>.Default.GetHashCode(LeftValue);
+                hashCode = (hashCode * 397) ^ EqualityComparer<TRight>.Default.GetHashCode(RightValue);
                 hashCode = (hashCode * 397) ^ (int)LeftOrRight;
                 return hashCode;
             }
         }
 
-        public static bool operator ==(in Either<L, R> a, in Either<L, R> b) => a.Equals(b);
+        public static bool operator ==(in Either<TLeft, TRight> a, in Either<TLeft, TRight> b) => a.Equals(b);
 
-        public static bool operator !=(in Either<L, R> a, in Either<L, R> b) => !a.Equals(b);
+        public static bool operator !=(in Either<TLeft, TRight> a, in Either<TLeft, TRight> b) => !a.Equals(b);
 
         public override string ToString()
         {
             var side = IsLeft ? "Left" : "Right";
             var value = IsLeft ? LeftValue.ToString() : RightValue.ToString();
-            return $"Either<{typeof(L).Name}, {typeof(R).Name}>({side} = {value})";
+            return $"Either<{typeof(TLeft).Name}, {typeof(TRight).Name}>({side} = {value})";
         }
     }
 
-    public enum LeftOrRight { Left, Right }
-
-    public static partial class Either
+    public static class Either
     {
         /* Match */
 
         // E<L,R> -> Out
-        public static Out Match<L, R, Out>(this Either<L, R> either, Func<L, Out> left, Func<R, Out> right) =>
+        public static TOut Match<TLeft, TRight, TOut>(this Either<TLeft, TRight> either, Func<TLeft, TOut> left, Func<TRight, TOut> right) =>
             either.IsLeft ? left(either.LeftValue) : right(either.RightValue);
 
         /* Map */
 
         // E<L,R> -> (L -> LOut, R -> ROut) -> E<LOut, ROut>
-        public static Either<LOut, ROut> Map<L, R, LOut, ROut>(this Either<L, R> either, Func<L, LOut> left, Func<R, ROut> right) =>
-            either.IsLeft ? ToEitherLeft<LOut, ROut>(left(either.LeftValue)) : ToEitherRight<LOut, ROut>(right(either.RightValue));
+        public static Either<TLeftOut, TRightOut> Map<TLeft, TRight, TLeftOut, TRightOut>(this Either<TLeft, TRight> either, Func<TLeft, TLeftOut> left, Func<TRight, TRightOut> right) =>
+            either.IsLeft ? ToEitherLeft<TLeftOut, TRightOut>(left(either.LeftValue)) : ToEitherRight<TLeftOut, TRightOut>(right(either.RightValue));
 
         /* Bind */
 
         // E<L, R> -> (L -> LOut, R -> ROut) -> E<LOut, ROut>
-        public static Either<LOut, ROut> Bind<L, R, LOut, ROut>(this Either<L, R> either, Func<L, Either<LOut, ROut>> left, Func<R, Either<LOut, ROut>> right) =>
+        public static Either<TLeftOut, TRightOut> Bind<TLeft, TRight, TLeftOut, TRightOut>(this Either<TLeft, TRight> either, Func<TLeft, Either<TLeftOut, TRightOut>> left, Func<TRight, Either<TLeftOut, TRightOut>> right) =>
             either.IsLeft ? left(either.LeftValue) : right(either.RightValue);
 
 
         /* Side effects */
 
         // E<L, R> -> (L -> Unit) -> E<L, R>
-        public static Either<L, R> DoWhenLeft<L, R>(this Either<L, R> either, Action<L> action)
+        public static Either<TLeft, TRight> DoWhenLeft<TLeft, TRight>(this Either<TLeft, TRight> either, Action<TLeft> action)
         {
             if (either.IsLeft)
                 action(either.LeftValue);
@@ -89,7 +89,7 @@ namespace Monacs.Core
         }
 
         // E<L, R> -> (R -> Unit) -> E<L, R>
-        public static Either<L, R> DoWhenRight<L, R>(this Either<L, R> either, Action<R> action)
+        public static Either<TLeft, TRight> DoWhenRight<TLeft, TRight>(this Either<TLeft, TRight> either, Action<TRight> action)
         {
             if (either.IsRight)
                 action(either.RightValue);
@@ -100,32 +100,32 @@ namespace Monacs.Core
         /* Factories */
 
         // L -> E<L, R>
-        public static Either<L, R> ToEitherLeft<L, R>(this L left) => new Either<L, R>(left);
+        public static Either<TLeft, TRight> ToEitherLeft<TLeft, TRight>(this TLeft left) => new Either<TLeft, TRight>(left);
 
         // R -> E<L, R>
-        public static Either<L, R> ToEitherRight<L, R>(this R right) => new Either<L, R>(right);
+        public static Either<TLeft, TRight> ToEitherRight<TLeft, TRight>(this TRight right) => new Either<TLeft, TRight>(right);
 
         // (L, R) -> E<L, R>
-        public static Either<L, R> ToEither<L, R>(this (L left, R right) tuple) =>
-            tuple.right == null ? ToEitherLeft<L, R>(tuple.left) : ToEitherRight<L, R>(tuple.right);
+        public static Either<TLeft, TRight> ToEither<TLeft, TRight>(this (TLeft left, TRight right) tuple) =>
+            tuple.right == null ? ToEitherLeft<TLeft, TRight>(tuple.left) : ToEitherRight<TLeft, TRight>(tuple.right);
 
         /* Collections */
 
         // List<E<L, R>> -> List<L>
-        public static IEnumerable<L> ChooseLeft<L, R>(this IEnumerable<Either<L, R>> items) =>
+        public static IEnumerable<TLeft> ChooseLeft<TLeft, TRight>(this IEnumerable<Either<TLeft, TRight>> items) =>
             items.Where(x => x.LeftOrRight == LeftOrRight.Left).Select(x => x.LeftValue);
 
         // List<E<L, R>> -> List<R>
-        public static IEnumerable<R> ChooseRight<L, R>(this IEnumerable<Either<L, R>> items) =>
+        public static IEnumerable<TRight> ChooseRight<TLeft, TRight>(this IEnumerable<Either<TLeft, TRight>> items) =>
             items.Where(x => x.LeftOrRight == LeftOrRight.Right).Select(x => x.RightValue);
 
         // List<E<L, R>> -> Option<List<L>>
-        public static Option<IEnumerable<L>> SequenceLeft<L, R>(this IEnumerable<Either<L, R>> items) =>
-            items.Any(x => x.IsRight) ? Option.None<IEnumerable<L>>() : Option.Some(items.ChooseLeft());
+        public static Option<IEnumerable<TLeft>> SequenceLeft<TLeft, TRight>(this IEnumerable<Either<TLeft, TRight>> items) =>
+            items.Any(x => x.IsRight) ? Option.None<IEnumerable<TLeft>>() : Option.Some(items.ChooseLeft());
 
         // List<E<L, R>> -> Option<List<R>>
-        public static Option<IEnumerable<R>> SequenceRight<L, R>(this IEnumerable<Either<L, R>> items) =>
-            items.Any(x => x.IsLeft) ? Option.None<IEnumerable<R>>() : Option.Some(items.ChooseRight());
+        public static Option<IEnumerable<TRight>> SequenceRight<TLeft, TRight>(this IEnumerable<Either<TLeft, TRight>> items) =>
+            items.Any(x => x.IsLeft) ? Option.None<IEnumerable<TRight>>() : Option.Some(items.ChooseRight());
 
     }
 }
